122f4d9f884a24ee6aa813e9020c9fe05feb96a0
Artem <proceller@yandex.ru>
2023-01-14 18:27:06 +0300

    pro 6

---
+++ project_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, Маша! Мы тебя ждали!");
+}
+else
+{
+    Console.Write("Привет, ");
+Console.WriteLine(username);
+}
