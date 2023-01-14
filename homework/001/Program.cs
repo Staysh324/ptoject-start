50e362069c231dc70df99d8d528f7e5de5f24aee
Artem <proceller@yandex.ru>
2023-01-14 19:53:33 +0300

    exe1

---
+++ homework/001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA<numberB)
+{
+    Console.WriteLine($"число {numberB} больше числа {numberA}");
+}
+else
+{
+    Console.WriteLine($"число {numberA} больше числа {numberB}");
+}
