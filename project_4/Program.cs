5567f14777ec08692730539254c45364ca9a15a2
Artem <proceller@yandex.ru>
2023-01-14 17:34:33 +0300

    pro4

---
+++ project_4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число 1");
+double NumberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+double NumberB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine (NumberA / NumberB);
